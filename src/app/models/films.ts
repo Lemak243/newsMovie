db322bebcb2a7889c273ee95526f1dc74fb7efd0
yves makwambala <yvesmak1@gmail.com>
2022-05-21 10:46:51 +0200

    création du modele

---
+++ src/app/models/films.ts
@@ -0,0 +1,12 @@
+export class Films {
+    id!: number;
+    ref_film!: string;
+    ref_users!: string;
+    images!: string;
+    titre!: string;
+    description!: string;
+    bande_annonce?: string;
+    acteur?: string;
+    status!: string;
+    sortie!: string;
+}
---
+++ src/app/models/users.ts
@@ -0,0 +1,7 @@
+export class Users {
+    id!: number;
+    ref_users!: string;
+    nom!: string;
+    prenom!: string;
+    date_creation!: Date;
+}
